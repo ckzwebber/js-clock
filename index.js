6def09f70e2ccaf23a9d70cb65c76c75246cf778
Carlos Miguel <85066001+ckzwebber@users.noreply.github.com>
2024-08-08 15:10:12 -0300

    Fix

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-let clock = document.getElementById("clock");
-let date = document.getElementById("date");
+const clock = document.getElementById("clock");
+const date = document.getElementById("date");
 
-function showTimeAndData() {
-  const daysOfWeek = [
-    "Domingo",
-    "Segunda",
-    "Terça",
-    "Quarta",
-    "Quinta",
-    "Sexta",
-    "Sábado",
-  ];
+function showTimeAndDate() {
+  const daysOfWeek = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
   const now = new Date();
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
-  const day = String(now.getDate()).padStart(2, "0");
-  const month = String(now.getMonth() + 1).padStart(2, "0");
+
+  const formatNumber = (number) => String(number).padStart(2, "0");
+
+  const hours = formatNumber(now.getHours());
+  const minutes = formatNumber(now.getMinutes());
+  const seconds = formatNumber(now.getSeconds());
+  const day = formatNumber(now.getDate());
+  const month = formatNumber(now.getMonth() + 1);
   const year = now.getFullYear();
-  const dayOfWeek = now.getDay();
+  const dayOfWeek = daysOfWeek[now.getDay()];
+
   clock.textContent = `${hours}:${minutes}:${seconds}`;
-  date.textContent = `${daysOfWeek[dayOfWeek]}, ${day}/${month}/${year}`;
+  date.textContent = `${dayOfWeek}, ${day}/${month}/${year}`;
 }
 
-const interval = setInterval(showTimeAndData, 1);
+setInterval(showTimeAndDate, 1000);
